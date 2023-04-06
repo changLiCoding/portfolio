b855249d3e31bd43498892f8865618c2a9e72efe
javascriptsucks <evix1988@live.cn>
2023-04-06 16:25:46 -0600

    change to fix empty page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), reactRefresh()],
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		sourcemap: false,
+		minify: "esbuild",
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
+		rollupOptions: {
+			input: "src/index.js",
+		},
+	},
+});
