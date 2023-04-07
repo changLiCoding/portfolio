b7a59aca824ea8b2731854a60df3d056c29fa853
javascriptsucks <evix1988@live.cn>
2023-04-06 19:08:28 -0600

    change homepage address

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		outDir: "dist",
-		assetsDir: "assets",
-		sourcemap: false,
-		minify: "esbuild",
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true,
-			},
-		},
-		rollupOptions: {
-			input: "src/main.jsx",
-		},
-	},
 });
