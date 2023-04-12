aa9d134fa42270c85905038884fb2a1276720e3b
javascriptsucks <evix1988@live.cn>
2023-04-12 10:17:50 -0600

    change of resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
 					</p>
 				</div>
 
-				<p className='text-md md:text-lg lg:text-xl mt-20'>
+				<p className='text-sm md:text-lg lg:text-xl mt-20'>
 					As a Full Stack Developer with over a year of experience in coding, I
 					have gained expertise in multiple programming languages, including
 					JavaScript, Ruby, Python, and TypeScript. Prior to my career in
@@ -23,14 +23,14 @@ export default function About() {
 
 				<br />
 
-				<p className='text-md md:text-lg lg:text-xl mt-20'>
+				<p className='text-sm md:text-lg lg:text-xl mt-20'>
 					My deep understanding of React and Redux allows me to develop robust
 					and efficient applications. I am constantly learning and staying
 					up-to-date with the latest technologies and industry trends, as I am
 					passionate about providing the best possible solutions for my clients
 					or customers.
 				</p>
-				<p className='text-md md:text-lg lg:text-xl '>
+				<p className='text-sm md:text-lg lg:text-xl '>
 					When I'm not coding, I enjoy playing video games and experimenting
 					with new recipes in the kitchen. I believe that these hobbies allow me
 					to approach problem-solving with a fresh perspective, and help me stay
