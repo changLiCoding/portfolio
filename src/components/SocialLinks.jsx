20fa97c22db77f087e2c420c80b0220c73dd1d8b
javascriptsucks <evix1988@live.cn>
2023-06-01 18:48:55 -0600

    update resume download route

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ export default function SocialLinks() {
 					Resume <BsFillPersonLinesFill size={30} />
 				</>
 			),
-			href: "/Resume Chang.pdf",
+			href: "/prtfolio/Resume Chang.pdf",
 			style: "rounded-br-md",
 			download: true,
 		},
